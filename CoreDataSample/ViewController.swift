1d25dab631672cf1b9bfcebaaae5d21a384c4cf4
Shrikant Kanakatti <kanakattishrikant@johndeere.com>
2022-11-03 09:41:40 +0530

    Initial Commit

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by Kanakatti Shrikant on 03/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
